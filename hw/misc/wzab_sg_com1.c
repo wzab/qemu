6d6419be555ba9df5766e7ccfb25dc3708c6b465
Wojciech M. Zabolotny <wzab01@gmail.com>
2019-11-30 01:10:41 +0100

    The device wzab_sg_com1 gets compiled.

--- hw/misc/wzab_sg_com1.c
+++ hw/misc/wzab_sg_com1.c
@@ -1,17 +1,17 @@
 /* This modell is not usable yet! It is currently under development. */
 
 /*
- * QEMU - Model of the device providing the trivial communication, using the 
+ * QEMU - Model of the device providing the trivial communication, using the
  * scatter-gather BM DMA.
- * 
+ *
  * When the DMA transfer is started, the model creates a new file,
  * and puts all received data into it.
- * 
+ *
  * How to create a file with the next number?
  * In the first approach we simply use the current directory.
  * We create files with numbers starting from 0 (so we will overwrite
  * files created in the previous session if any).
- * 
+ *
  * Copyright Wojciech M. Zabolotny ( wzab@ise.pw.edu.pl ) 2011-2019
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -33,14 +33,14 @@
  * THE SOFTWARE.
  */
 
-#define DEBUG_wzab1 1 
+#define DEBUG_wzab1 1
 
 //PCI IDs below are not registred! Use only for experiments!
 #define PCI_VENDOR_ID_WZAB 0xabba
 #define PCI_DEVICE_ID_WZAB_WZCOM1 0x0125
 
 //Simulated processing time
-#define ENC1_PROCESSING_TIME 5000000
+#define COM1_PROCESSING_TIME 1000
 #include "qemu/osdep.h"
 #include "qemu/compiler.h"
 #include <string.h>
@@ -50,13 +50,13 @@
 #include "hw/pci/pci.h"
 #include <mcrypt.h>
 #include "qemu/timer.h"
-#include "wzab_enc1.h"
-
+#include "wzab_sg_com1.h"
+#include <stdio.h>
 /*
   Description of functionalities.
   The device simply takes the SG buffer described by the descriptors organized in a list:
   The list of descriptors may contain single buffer descriptors or array of descriptors.
-  
+
   Offset   - Meaning
   0          Number of buffers
   1          Next descriptor
@@ -75,283 +75,268 @@
 */
 
 //Some prototypes...
-static uint64_t pci_wz_enc1_read(void *opaque, hwaddr addr, unsigned size);
-static void pci_wz_enc1_write(void *opaque, hwaddr addr, uint64_t val, unsigned size);
-int wz_enc1_init (PCIBus *bus);
+static uint64_t pci_wz_com1_read(void *opaque, hwaddr addr, unsigned size);
+static void pci_wz_com1_write(void *opaque, hwaddr addr, uint64_t val, unsigned size);
+int wz_com1_init (PCIBus *bus);
 
-typedef struct WzEnc1State {
-  /*<private>*/
-  PCIDevice parent_obj;
-  /*<public>*/
-  MemoryRegion mmio;
-  union {
-    WzEnc1Regs r;
-    uint32_t u32[sizeof(WzEnc1Regs)/sizeof(uint32_t)];
-  } regs;
-  uint32_t wz_enc1_mmio_io_addr;
-  uint32_t irq_pending;
-  uint32_t irq_mask;
-  uint8_t Error;
-  uint8_t Working;
-  MCRYPT td;
-  uint8_t enc_ndec;
-  QEMUTimer * timer;
-} WzEnc1State;
+typedef struct WzCom1State
+{
+    /*<private>*/
+    PCIDevice parent_obj;
+    /*<public>*/
+    MemoryRegion mmio;
+    union
+    {
+        WzCom1Regs r;
+        com1_addr_t ureg[sizeof(WzCom1Regs)/sizeof(com1_addr_t)];
+    } regs;
+    com1_addr_t wz_com1_mmio_io_addr;
+    uint8_t irq_pending;
+    uint8_t irq_mask;
+    uint8_t Error;
+    uint8_t Working;
+    QEMUTimer * timer;
+} WzCom1State;
 
-#define TYPE_PCI_WZENC1 "pci-wzenc1"
- 
-#define PCI_WZENC1(obj) \
-OBJECT_CHECK(WzEnc1State, (obj), TYPE_PCI_WZENC1) 
-static const MemoryRegionOps pci_wz_enc1_mmio_ops = {
-    .read = pci_wz_enc1_read,
-    .write = pci_wz_enc1_write,
+#define TYPE_PCI_WZCOM1 "pci-wzcom1"
+
+#define PCI_WZCOM1(obj) \
+    OBJECT_CHECK(WzCom1State, (obj), TYPE_PCI_WZCOM1)
+static const MemoryRegionOps pci_wz_com1_mmio_ops =
+{
+    .read = pci_wz_com1_read,
+    .write = pci_wz_com1_write,
     .endianness = DEVICE_LITTLE_ENDIAN,
     .impl = {
-        .min_access_size = 4, //Always 32-bit access!!
-        .max_access_size = 4,
+        .min_access_size = 8, //Always 32-bit access!!
+        .max_access_size = 8,
     },
 };
 
 
-static void wz_enc1_reset (void * opaque)
+static void wz_com1_reset (void * opaque)
 {
-  WzEnc1State *s = opaque;
-  memset((void *)s->regs.u32,0,sizeof(s->regs.u32));
+    WzCom1State *s = opaque;
+    memset((void *)s->regs.ureg,0,sizeof(s->regs.ureg));
 #ifdef DEBUG_wzab1
-  printf("wzab_tst1 reset!\n");
+    printf("wzab_com1 reset!\n");
 #endif
 }
 
 /* called for read accesses to our register memory area */
-static uint64_t pci_wz_enc1_read(void *opaque, hwaddr addr, unsigned size)
+static uint64_t pci_wz_com1_read(void *opaque, hwaddr addr, unsigned size)
 {
-  int i_addr;
+    int i_addr;
 #ifdef DEBUG_wzab1
-  printf("Memory read: address %x\n ", (unsigned int) addr);
+    printf("Memory read: address %x\n ", (unsigned int) addr);
 #endif
-  WzEnc1State *s = opaque;
-  if(addr>=sizeof(WzEnc1Regs)){
-    //Read above registers area - return "0xbada4ea" to show it
-    return 0xbada4ea;
-  } else if (addr == offsetof(WzEnc1Regs, Status)) {
-    //Reading of the Status - synthesize the correct value
-    uint32_t res = s->Error & 0xffff ; //Lower 16 bits!
-    if (s->Working) res |= 0x1000;
-    if (s->irq_pending) res |= 0x8000;
-    if (s->irq_mask) res |= 0x4000;
-#ifdef DEBUG_wzab1
-    printf("Status=%d\n",res);
-#endif
-    return res;
-  }else {
-    i_addr = addr/4;
-    //Read the register
-    return s->regs.u32[i_addr];
-  }
-}
-
-static char cipher[] = "rijndael-256";
-static char cipher_mode[] = "cbc";
-
-/* called for write accesses to our register memory area */
-
-static void pci_wz_enc1_write(void *opaque, hwaddr addr, uint64_t val, unsigned size)
-{
-  WzEnc1State *s = opaque;
-  int i_addr;
-#ifdef DEBUG_wzab1  
-  printf("wzab1: zapis pod adres = 0x%016" PRIu64 " , 0x%016" PRIu64 "\n", (uint64_t) addr, val);
-#endif
-  /* Check which register is accessed */
-  if(addr>=sizeof(WzEnc1Regs)){
-    //Write above registers area - ignore it!
-  } else {
-    i_addr = addr/4;
-    //Write the value
-    s->regs.u32[i_addr]=val;
-    //Interprete its special meaning
-    if (addr==offsetof(WzEnc1Regs,Ctrl)) {
-      //Write to control register - we need to check what operation is required
-      switch(val) {
-      case ENC1_CMD_DECR:
-      case ENC1_CMD_ENCR: {
-	//required initialization of the encryption engine
-	//key and IV provided
-	uint8_t key[32];
-	uint8_t iv[32];
-	s->td = mcrypt_module_open(cipher, NULL, cipher_mode, NULL);
-	if(s->td == NULL) {
-	  printf("Initialization A of mcrypt impossible!\n");
-	  exit(1);
-	}
-	if (val==ENC1_CMD_ENCR) {
-	  s->enc_ndec = 1; //We will encrypt data
-	} else {
-	  s->enc_ndec = 0; //We will decrypt data
-	}
-        pci_dma_read(&s->parent_obj,s->regs.r.Pages[0].PhysAddr,key,32);
-        pci_dma_read(&s->parent_obj,s->regs.r.Pages[1].PhysAddr,iv,32);
-	if(mcrypt_generic_init(s->td,(void*)key,32,(void*)iv)<0){
-	  printf("Initialization B of mcrypt impossible!\n");
-	  exit(1);
-	}
-        break;
-      }
-      case ENC1_CMD_DATA:
-	//next set of data provided, no reinitialization of encryption engine
-	if(s->td == NULL) {
-	  //Error - hardware not initialized
-	  s->Error = ENC1_ERR_NOTINIT; //Error hardware not initialized
-	  s->irq_pending = 1;
-	  if(s->irq_mask==0) pci_irq_assert(&(s->parent_obj));
-	} else if(s->Working) {
-	  //Error - engine is still busy!
-	  s->Error = ENC1_ERR_BUSY; //Error!
-	  s->irq_pending = 1;
-	  if(s->irq_mask==0) pci_irq_deassert(&(s->parent_obj));
-	} else {
-	  //Normal operation - submit data to processing
-	  s->Working = 0x1;
-	  timer_mod(s->timer,qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL)+ENC1_PROCESSING_TIME);
-	}
-	break;
-      case ENC1_CMD_STOP:
-	//End of encryption!
-	mcrypt_generic_deinit(s->td);
-	mcrypt_module_close(s->td);
-	s->td = NULL;
-	s->irq_pending = 0;
-	s->irq_mask = 0;
-	pci_irq_deassert(&(s->parent_obj));
-	break;
-      case ENC1_CMD_ACKIRQ:
-	//Confirm reception of interrupt (and unmask it)
-	s->irq_pending = 0;
-	s->irq_mask = 0;
-	pci_irq_deassert(&(s->parent_obj));
-	break;
-      case ENC1_CMD_DISIRQ:
-	//Mask interrupt
-	s->irq_mask = 1;
-	pci_irq_deassert(&(s->parent_obj));
-	break;
-      case ENC1_CMD_ENAIRQ:
-	//Unmask interrupt
-	s->irq_mask = 0;
-	if(s->irq_pending) pci_irq_assert(&(s->parent_obj));
-	break;
-      }
+    WzCom1State *s = opaque;
+    if(addr>=sizeof(WzCom1Regs))
+    {
+        //Read above registers area - return "0xbada4ea" to show it
+        return 0xbada4ea;
     }
-  }
-}
-
-/* New procedures developed for WZCOM1 */
-
-
-/* The procedure below performs the real encryption, after simulated processing time is expired */
-static void wzab1_tick(void *opaque)
-{ 
-  WzEnc1State * s = opaque;
-  //Encrypt the data
-  {
-    uint8_t block[32];
-    int p;
-    for(p=0;p<WZENC1_NOF_PAGES;p++) {
-      //Loop through all pages
-      int i;
-      int j = s->regs.r.Pages[p].PhysAddr + s->regs.r.Pages[p].Offset;
-      int len = s->regs.r.Pages[p].Length ;
-      if(!len) break; //Page with Length=0 is found
-      for(i=0; i<len ; i+=32) {
-	//Read data from page to processing buffer
-	pci_dma_read(&s->parent_obj,j+i,block,32);
-	if (s->enc_ndec) {
-	  //Encrypt data
-	  mcrypt_generic (s->td, block, 32);
-	} else {
-	  //Decrypt data
-	  mdecrypt_generic (s->td, block, 32); 
-	}
-	//Write processed data back
-	pci_dma_write(&s->parent_obj,j+i,block,32);
-      }
-    }
-  }
+    else if (addr == offsetof(WzCom1Regs, Status))
+    {
+        //Reading of the Status - synthesize the correct value
+        uint32_t res = s->Error & 0xffff ; //Lower 16 bits!
+        if (s->Working)
+            res |= 0x1000;
+        if (s->irq_pending)
+            res |= 0x8000;
+        if (s->irq_mask)
+            res |= 0x4000;
 #ifdef DEBUG_wzab1
-  printf("Data processed - request IRQ!\n");
+        printf("Status=%d\n",res);
 #endif
-  s->Working = 0;
-  s->irq_pending = 1;
-  if(s->irq_mask==0) pci_irq_assert(&(s->parent_obj));
+        return res;
+    }
+    else
+    {
+        i_addr = addr/sizeof(com1_addr_t);
+        //Read the register
+        return s->regs.ureg[i_addr];
+    }
 }
 
-static void pci_wzenc1_realize (PCIDevice *pdev, Error **errp)
+
+/* The function that performs the DMA transfer */
+static void wz_com1_do_dma(void *opaque)
 {
-  WzEnc1State *s = PCI_WZENC1(pdev);
-  uint8_t *c = s->parent_obj.config;
-  
-  /* TODO: RST# value should be 0. */
-  c[PCI_INTERRUPT_PIN] = 1;
-  //Register memory mapped registers
-  memory_region_init_io(&s->mmio,OBJECT(s),&pci_wz_enc1_mmio_ops,s,
-                        "pci-wzenc1-mmio", 0x100); //@@sizeof(s->regs.u32));
-  pci_register_bar (&s->parent_obj, 0,  PCI_BASE_ADDRESS_SPACE_MEMORY,&s->mmio);
-  qemu_register_reset (wz_enc1_reset, s);
-  //Register timer used to simulate processing time
-  s->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, wzab1_tick, s);
-  wz_enc1_reset (s);
-  //return 0;
+    WzCom1State * s = opaque;
+    //If the first descriptor is NULL, return
+    if(!s->regs.r.FirstDesc)
+        return;
+    //Open or create the file for writing
+    FILE * fout = fopen("dma_out.bin","w");
+    //Set the run flag
+    s->Working = 1;
+    //Read the first descriptor
+    s->regs.r.CurDesc = s->regs.r.FirstDesc;
+    while(1)
+    {
+        //Here we start processing of the current descriptor
+        com1_addr_t n_entries, next;
+        //Read the number of entries
+        pci_dma_read(&s->parent_obj,s->regs.r.CurDesc+offsetof(Com1Desc, Length), &next, sizeof(com1_addr_t));
+        //Read the address of the next descriptor
+        pci_dma_read(&s->parent_obj,s->regs.r.CurDesc+offsetof(Com1Desc, Next), &n_entries, sizeof(com1_addr_t));
+        com1_addr_t bufs = s->regs.r.CurDesc+offsetof(Com1Desc, Descs);
+        //Start processing of subsequent buffers in the descriptor
+        for(int i=0; i<n_entries; i++)
+        {
+            BufDesc cur_buf;
+            pci_dma_read(&s->parent_obj, bufs+i*sizeof(BufDesc), &cur_buf, sizeof(cur_buf));
+            //Now we receive the data from the buffer
+            for(int j=0; j < cur_buf.Length; j++)
+            {
+                //It is inefficient, but simple - let's copy it byte by byte.
+                uint8_t b;
+                pci_dma_read(&s->parent_obj, cur_buf.BufAddr+cur_buf.Offset+j,&b,1);
+                fwrite(&b,1,1,fout);
+            }
+        }
+        //All entries serviced, let's go to the next descriptor;
+        if(!next)
+        {
+            //That was the last descriptor
+            fclose(fout);
+            break;
+        }
+        else
+        {
+            s->regs.r.CurDesc = next;
+        }
+    }
+    //Signal that the work is done and raise the IRQ
+    s->Working = 0;
+    s->irq_pending = 1;
+    if (!s->irq_mask)
+        pci_irq_assert(&(s->parent_obj));
+    return;
 }
 
-static void pci_wz_enc1_init(Object *obj)
+static void pci_wz_com1_write(void *opaque, hwaddr addr, uint64_t val, unsigned size)
+{
+    WzCom1State *s = opaque;
+    int i_addr;
+#ifdef DEBUG_wzab1
+    printf("wzab1: zapis pod adres = 0x%016" PRIu64 " , 0x%016" PRIu64 "\n", (uint64_t) addr, val);
+#endif
+    /* Check which register is accessed */
+    if(addr>=sizeof(WzCom1Regs))
+    {
+        //Write above registers area - ignore it!
+    }
+    else
+    {
+        i_addr = addr/sizeof(com1_addr_t);
+        //Write the value
+        s->regs.ureg[i_addr]=val;
+        //Interprete its special meaning
+        if (addr==offsetof(WzCom1Regs,Ctrl))
+        {
+            //Write to control register - we need to check what operation is required
+            switch(val)
+            {
+            case COM1_CMD_START:
+                //End of encryption!
+                s->irq_pending = 0;
+                pci_irq_deassert(&(s->parent_obj));
+                //Start processing in a separate thread via timer
+                timer_mod_anticipate(s->timer,qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL)+COM1_PROCESSING_TIME);
+                break;
+            case COM1_CMD_ACKIRQ:
+                //Confirm reception of interrupt (and unmask it)
+                s->irq_pending = 0;
+                s->irq_mask = 0;
+                pci_irq_deassert(&(s->parent_obj));
+                break;
+            case COM1_CMD_DISIRQ:
+                //Mask interrupt
+                s->irq_mask = 1;
+                pci_irq_deassert(&(s->parent_obj));
+                break;
+            case COM1_CMD_ENAIRQ:
+                //Unmask interrupt
+                s->irq_mask = 0;
+                if(s->irq_pending)
+                    pci_irq_assert(&(s->parent_obj));
+                break;
+            }
+        }
+    }
+}
+
+static void pci_wz_com1_realize (PCIDevice *pdev, Error **errp)
+{
+    WzCom1State *s = PCI_WZCOM1(pdev);
+    uint8_t *c = s->parent_obj.config;
+
+    /* TODO: RST# value should be 0. */
+    c[PCI_INTERRUPT_PIN] = 1;
+    //Register memory mapped registers
+    memory_region_init_io(&s->mmio,OBJECT(s),&pci_wz_com1_mmio_ops,s,
+                          "pci-wzcom1-mmio", 0x100); //@@sizeof(s->regs.u32));
+    pci_register_bar (&s->parent_obj, 0,  PCI_BASE_ADDRESS_SPACE_MEMORY,&s->mmio);
+    qemu_register_reset (wz_com1_reset, s);
+    //Register timer used to simulate processing time
+    s->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, wz_com1_do_dma, s);
+    wz_com1_reset (s);
+    //return 0;
+}
+
+static void pci_wz_com1_init(Object *obj)
 {
 }
 static void
-pci_wzenc1_uninit(PCIDevice *dev)
+pci_wz_sg_com1_uninit(PCIDevice *dev)
 {
-    WzEnc1State *d = PCI_WZENC1(dev);
+    WzCom1State *d = PCI_WZCOM1(dev);
 
-    wz_enc1_reset(d);
+    wz_com1_reset(d);
     timer_free(d->timer);
 }
 
-static void qdev_pci_wzenc1_reset(DeviceState *dev)
+static void qdev_pci_wz_com1_reset(DeviceState *dev)
 {
-    WzEnc1State *d = PCI_WZENC1(dev);
-    wz_enc1_reset(d);
+    WzCom1State *d = PCI_WZCOM1(dev);
+    wz_com1_reset(d);
 }
 
-static void pci_wzenc1_class_init(ObjectClass *klass, void *data)
+static void pci_wz_com1_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
     PCIDeviceClass *k = PCI_DEVICE_CLASS(klass);
-    k->realize = pci_wzenc1_realize;
-    k->exit = pci_wzenc1_uninit;
+    k->realize = pci_wz_com1_realize;
+    k->exit = pci_wz_sg_com1_uninit;
     k->vendor_id = PCI_VENDOR_ID_WZAB;
-    k->device_id = PCI_DEVICE_ID_WZAB_WZENC1;
+    k->device_id = PCI_DEVICE_ID_WZAB_WZCOM1;
     k->revision = 0x00;
     k->class_id = PCI_CLASS_OTHERS;
-    dc->desc = "PCI demo AES accelerator";
+    dc->desc = "PCI SG DMA demonstrator";
     set_bit(DEVICE_CATEGORY_MISC, dc->categories);
-    dc->reset = qdev_pci_wzenc1_reset;
+    dc->reset = qdev_pci_wz_com1_reset;
 }
 
-static void pci_wzenc1_register_types(void)
+static void pci_wz_com1_register_types(void)
 {
-    static InterfaceInfo interfaces[] = {
+    static InterfaceInfo interfaces[] =
+    {
         { INTERFACE_CONVENTIONAL_PCI_DEVICE },
         { },
     };
-    static const TypeInfo pci_wzenc1_info = {
-    	.name          = TYPE_PCI_WZENC1,
-    	.parent        = TYPE_PCI_DEVICE,
-    	.instance_size = sizeof(WzEnc1State),
-    	.instance_init = pci_wz_enc1_init,
-    	.class_init    = pci_wzenc1_class_init,
+    static const TypeInfo pci_wz_com1_info =
+    {
+        .name          = TYPE_PCI_WZCOM1,
+        .parent        = TYPE_PCI_DEVICE,
+        .instance_size = sizeof(WzCom1State),
+        .instance_init = pci_wz_com1_init,
+        .class_init    = pci_wz_com1_class_init,
         .interfaces = interfaces,
-};
-    type_register_static(&pci_wzenc1_info);
+    };
+    type_register_static(&pci_wz_com1_info);
 }
 
-type_init(pci_wzenc1_register_types)
+type_init(pci_wz_com1_register_types)
 
--- hw/misc/wzab_sg_com1.h
+++ hw/misc/wzab_sg_com1.h
@@ -1,7 +1,7 @@
 //It would be good to prepare it in the form, where
 //we can easily switch between th 32-bit and 64-bit implementations.
 
-typedef uint32_t com1_addr_t;
+typedef uint64_t com1_addr_t;
 
 //The structure below describes a single buffer
 typedef struct BufDesc {
@@ -19,23 +19,22 @@ typedef struct Com1Desc {
 } __attribute__((packed)) Com1Desc;
 
 typedef struct WzCom1Regs {
-    uint32_t Ctrl;
-    uint32_t Status;
+    com1_addr_t Id; // To check that the device is here and is accessible
+    com1_addr_t Test; // To check that we can write and read the register
+    com1_addr_t Ctrl;
+    com1_addr_t Status;
     com1_addr_t FirstDesc;
     com1_addr_t CurDesc; //Currently processed descriptor
     com1_addr_t CurBuf; //Number of the currently processed buffer
     com1_addr_t CurPos; //Currently processed position in the buffer
-    com1_addr_t CurCount; //Number of bytes processed in the current transfer 
+    com1_addr_t CurCount; //Number of bytes processed in the current transfer
 } __attribute__((packed)) WzCom1Regs;
 
 //Commands
-#define COM1_CMD_DECR 1
-#define COM1_CMD_ENCR 2
-#define COM1_CMD_DATA 3
-#define COM1_CMD_STOP 4
-#define COM1_CMD_ENAIRQ 5
-#define COM1_CMD_DISIRQ 6
-#define COM1_CMD_ACKIRQ 7
+#define COM1_CMD_START 1
+#define COM1_CMD_ENAIRQ 2
+#define COM1_CMD_DISIRQ 3
+#define COM1_CMD_ACKIRQ 4
 
 //Errors
 #define COM1_ERR_NOTINIT 1
