42714bd2817be8db65e32c2040f7cc7c1df3f27b
Wojciech Zabołotny <wzab@WZabHP.nasz.dom>
2021-04-08 09:54:52 +0200

    Defined the registers for the model of the DMA engine.

--- hw/misc/wzab_daq1.h
+++ hw/misc/wzab_daq1.h
@@ -1,11 +1,20 @@
-//Definitions of 32-bit registers
-#define TST1_DIV  	0
-#define TST1_READP	1
-#define TST1_WRITEP	2
-#define TST1_CTRL       3
-#define TST1_PAGE1	4
-#define TST1_PAGE2	5
-#define TST1_PAGE3	6
-#define TST1_PAGE4	7
-//Number of registers
-#define TST1_REGS_NUM	8
+// Definitions of 64-bit registers
+// They are located at the begining of the BAR0
+#define DAQ1_DIV  	0
+#define DAQ1_READP	1
+#define DAQ1_WRITEP	2
+#define DAQ1_CTRL       3
+#define DAQ1_STAT	4
+//Address of the huge page with descriptors of events
+#define DAQ1_EVTS	5
+
+//Number of the 64-bit registers
+#define DAQ1_REGS_NUM	6
+
+// The memory that stores the huge pages forming the data buffer is located in BAR0 at offset DAQ1_BUFF_OFFS
+// We want to align it to its size. Now we assume that 8192 buffers will be sufficient, so the offset is set to:
+// 8*8192 = 8 * 0x2000 = 0x10000
+#define DAQ1_BUFS	0x10000
+// Number of the buffers:
+#define DAQ1_NBUFS	0x2000
+
