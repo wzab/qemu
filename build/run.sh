a8901218b8cdbcf673d06c7a4b49032f31c86530
Wojciech M. Zabolotny <wzab01@gmail.com>
2021-04-11 18:59:32 +0200

    Script run.sh modified to mount the /tmp/p9 directory as a shared one.

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,6 @@
-/tmp/qemu/bin/qemu-system-x86_64 -M q35 -m 16384 -cdrom KNOPPIX_V9.1DVD-2021-01-25-EN.iso --enable-kvm --device pci-wzdaq1
+/tmp/qemu/bin/qemu-system-x86_64 -M q35 -m 16384 \
+  -cdrom KNOPPIX_V9.1DVD-2021-01-25-EN.iso \
+  --enable-kvm \
+  --device pci-wzdaq1 \
+  -virtfs local,path=/tmp/p9,mount_tag=host0,security_model=none,id=host0
+
