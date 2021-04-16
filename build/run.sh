f8c7c3ab83f7167a1f3ff451da8f90e75281f320
Wojciech M. Zabolotny <wzab01@gmail.com>
2021-04-16 18:41:54 +0200

    Removed wzab_daq1_tmp.c . Modified run.sh to work with the Debian/testing image

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,7 @@
 /tmp/qemu/bin/qemu-system-x86_64 -M q35 -m 16384 \
-  -cdrom KNOPPIX_V9.1DVD-2021-01-25-EN.iso \
+  -hda hda.img \
   --enable-kvm \
   --device pci-wzdaq1 \
   -virtfs local,path=/tmp/p9,mount_tag=host0,security_model=none,id=host0
 
+#  -cdrom debian-testing-amd64-netinst.iso 
